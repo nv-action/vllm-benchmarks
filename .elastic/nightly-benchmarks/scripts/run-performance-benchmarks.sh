463f6b85d1305dbc87140413fa27263c43969da1
wangli <wangli858794774@gmail.com>
2025-03-18 14:52:39 +0800

    test new ci
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- .elastic/nightly-benchmarks/scripts/run-performance-benchmarks.sh
+++ .elastic/nightly-benchmarks/scripts/run-performance-benchmarks.sh
@@ -291,7 +291,6 @@ main() {
   run_latency_tests $QUICK_BENCHMARK_ROOT/tests/latency-tests.json
   run_throughput_tests $QUICK_BENCHMARK_ROOT/tests/throughput-tests.json
 
-  send_to_es   $COMMIT_ID "$COMMIT_TITLE" "$COMMIT_TIME"
 
   rm -rf $RESULTS_FOLDER
 
