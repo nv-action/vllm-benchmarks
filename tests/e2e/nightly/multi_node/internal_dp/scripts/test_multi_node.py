f5dd678c0e48d906a3925add80ca34d9307efe94
xqchen7 <chenxueqing7@huawei.com>
2026-08-12 09:46:20 +0800

    Update test_multi_node.py

--- tests/e2e/nightly/multi_node/internal_dp/scripts/test_multi_node.py
+++ tests/e2e/nightly/multi_node/internal_dp/scripts/test_multi_node.py
@@ -154,6 +154,7 @@ def _save_benchmark_results_json(config: MultiNodeConfig, results: list[Any]) ->
 @pytest.mark.asyncio
 async def test_multi_node() -> None:
     config = MultiNodeConfigLoader.from_yaml()
+    config.envs["VLLM_USE_MODELSCOPE"] = "False"
     if config.special_dependencies:
         for k, v in config.special_dependencies.items():
             command = [
